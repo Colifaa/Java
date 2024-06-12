ac1c02ec1ee4a06658be109cfecd02324fc19802
unknown <Jorgemathez14@gmail.com>
2024-06-12 16:08:19 -0300

    Ejercicio 2

---
+++ Ejercicio2.java
@@ -0,0 +1,19 @@
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        // EJERCICIOS 2.3, 2.5, 2.6 y 2.7 de la CLASE 3
+        var miVariableEntera2 = 10;
+        var miVariableCadena2 = "Seguimos estudiandooooooo";
+        System.out.println("miVariableEntera2 = " + miVariableEntera2);
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+
+        var usuario = "Osvaldo";
+        var titulo = "Ingeniero";
+        var union = titulo + usuario;
+
+        var a = 8;
+        var b = 4;
+        System.out.println(usuario + (a + b));
+
+    }
+}
